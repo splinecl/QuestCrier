afd533597ecaf559dd136e3f04b5454c35b208ac
Timo Leinonen <splinecl@gmail.com>
2019-10-21 14:20:05 +0200

    Handle ADDON_LOADED event
    Handle ADDON_LOADED event

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
-print("Quest Crier @project-version@");
-
 local debug = false;
 local frame = CreateFrame("FRAME", "QuestCrierAddonFrame");
 
 frame:RegisterEvent("UI_INFO_MESSAGE");
+frame:RegisterEvent("ADDON_LOADED");
 frame:SetScript("OnEvent", eventHandler);
 
 SLASH_QUEST_CRIER1 = "/qc"
@@ -20,19 +19,26 @@ function eventHandler(self, event, ...)
     local arg1, arg2 = ...;
     local isPlayerInParty = UnitInParty("player");
 
-    --287 Killed quest mob/Picked up quest item
-    --288 ?
-    --289 Picked up quest item
-    if (event == "UI_INFO_MESSAGE") then
-        --We want to default to true if this is a first time load
+    if(event == "ADDON_LOADED") then
+         --We want to default to true if this is a first time load
         if(isEnabled == nil) then
             isEnabled = true;
         end
+        print("Quest Crier @project-version@ toggle on/off using /gc");
+    end
+    
+    if (event == "UI_INFO_MESSAGE") then
+
         if(debug) then
             print("[QuestCrier arg1] " .. arg1);
             print("[QuestCrier arg2] " .. arg2);
         end
-
+        
+        --285 Scout/investigate area
+        --286 ?
+        --287 Killed quest mob/Picked up quest item
+        --288 ?
+        --289 Picked up quest item
         if(isEnabled and isPlayerInParty and (arg1 == 287 or arg1 == 288 or arg1 == 289)) then
             SendChatMessage(arg2, "PARTY");
         end
