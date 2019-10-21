2e09bab1c2c9dd0468821fe3e4afec40876fdc05
Timo Leinonen <splinecl@gmail.com>
2019-10-21 19:42:18 +0200

    Enable option to throttle progress messages

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ This enables party members to easily track the progress of each others quests.
 ## Slash Commands
 
 Use `/qc` to toggle addon on/off
+Use `/qc <number>` to toggle throttling
+
+### Throttling
+If throttling is set to X, you will only see every X progress message. You will always see the last message no matter what throttling is set to.
+
+So if throttle is set to 5 the quest "Dwarven Outiftters" would output the following.
+
+    Tough Wolf Meat: 5/8
+    Tough Wolf Meat: 8/8
 
 ## CurseForge link
 https://www.curseforge.com/wow/addons/quest-crier
--- main.lua
+++ main.lua
@@ -4,13 +4,20 @@ local frame = CreateFrame("FRAME", "QuestCrierAddonFrame");
 frame:RegisterEvent("UI_INFO_MESSAGE");
 frame:RegisterEvent("ADDON_LOADED");
 
-SLASH_QUEST_CRIER1 = "/qc"
+SLASH_QUEST_CRIER1 = "/qc";
 SlashCmdList["QUEST_CRIER"] = function(msg)
-    isEnabled = not isEnabled;
-    if(isEnabled) then
-        DEFAULT_CHAT_FRAME:AddMessage("Quest Crier on", 1.0, 1.0, 0.0);
+    local msgNumber = tonumber(msg);
+
+    if(type(msgNumber) == 'number') then
+        throttle = tonumber(msg);
+        DEFAULT_CHAT_FRAME:AddMessage("Quest Crier throttle set to "..throttle, 1.0, 1.0, 0.0);
     else
-        DEFAULT_CHAT_FRAME:AddMessage("Quest Crier off", 1.0, 1.0, 0.0);
+        isEnabled = not isEnabled;
+        if(isEnabled) then
+            DEFAULT_CHAT_FRAME:AddMessage("Quest Crier on", 1.0, 1.0, 0.0);
+        else
+            DEFAULT_CHAT_FRAME:AddMessage("Quest Crier off", 1.0, 1.0, 0.0);
+        end
     end
 end 
 
@@ -20,15 +27,21 @@ function eventHandler(self, event, ...)
     if(event == "ADDON_LOADED" and arg1 == "QuestCrier") then
          --We want to default to true if this is a first time load
         if(isEnabled == nil) then
-            printDebug("First time load");
             isEnabled = true;
+            printDebug("Initializing isEnalbed to true");
         end
-        DEFAULT_CHAT_FRAME:AddMessage("Quest Crier @project-version@ toggle on/off using /qc, currently "..(isEnabled and 'on' or 'off'), 1.0, 1.0, 0.0, true);
+        --We want to default to 1 in throttle if this is a first time load
+        if(throttle == nil) then
+            throttle = 1;
+            printDebug("Initializing throttle to 1");
+        end
+        DEFAULT_CHAT_FRAME:AddMessage("Quest Crier @project-version@ toggle on/off using /qc, currently "..(isEnabled and 'on' or 'off').." throttle at "..throttle, 1.0, 1.0, 0.0, true);
         frame:UnregisterEvent("ADDON_LOADED");
     end
     
     if (event == "UI_INFO_MESSAGE") then
         local isPlayerInParty = UnitInParty("player");
+
         printDebug("[QuestCrier arg1] " .. arg1);
         printDebug("[QuestCrier arg2] " .. arg2);
         
@@ -38,8 +51,24 @@ function eventHandler(self, event, ...)
         --288 ?
         --289 Picked up quest item
         if(isEnabled and isPlayerInParty and (arg1 == 285 or arg1 == 286 or arg1 == 287 or arg1 == 288 or arg1 == 289)) then
-            SendChatMessage(arg2, "PARTY");
+            handleQuestMessage(arg2);
         end
+    end
+end
+
+function handleQuestMessage(message)
+    local current, total = string.match(message, ": (%d+)/(%d+)");
+    printDebug("current: "..current);
+    printDebug("total: "..total);
+    printDebug("throttle: "..throttle);
+    if(current and throttle) then
+        if((current % throttle) == 0 or current == total) then
+            SendChatMessage(message, "PARTY");
+            printDebug("current % throttle "..message);
+        end
+    else
+        SendChatMessage(message, "PARTY");
+        printDebug("NOT current and throttle "..message);
     end
 end
 
